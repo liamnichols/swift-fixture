0321d040b1758758b1c0c8ea1e5971b30d3cff0c
Liam Nichols <liam.nichols.ln@gmail.com>
2023-06-26 22:10:34 +0200

    More documentation

--- Sources/SwiftFixture/PreferredFormat.swift
+++ Sources/SwiftFixture/PreferredFormat.swift
@@ -1,7 +1,10 @@
+/// The preferred format of fixture values provided by ``Fixture``.
+///
+/// - Note: This value is only guaranteed to influence the default registered types and may be ignored elsewhere.
 public enum PreferredFormat {
-    /// Prefer random fixture values
+    /// Prefer random fixture values.
     case random
 
-    /// Prefer constant fixture values for a given type
+    /// Prefer constant fixture values for a given type.
     case constant
 }
