c0ec13bdd4bdf6ab3c7e773678cc5bbe7dafa3d0
Liam Nichols <liam.nichols.ln@gmail.com>
2023-06-26 11:34:00 +0200

    Remove GitHub Pages documentation and configure .spi.yml to build docs as part of SPI

--- Package.swift
+++ Package.swift
@@ -1,6 +1,7 @@
 // swift-tools-version: 5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+import Foundation
 import PackageDescription
 
 let package = Package(
@@ -8,11 +9,16 @@ let package = Package(
     products: [
         .library(name: "SwiftFixture", targets: ["SwiftFixture"])
     ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0"),
-    ],
     targets: [
         .target(name: "SwiftFixture"),
         .testTarget(name: "SwiftFixtureTests", dependencies: ["SwiftFixture"])
     ]
 )
+
+// https://swiftpackageindex.com/swiftpackageindex/spimanifest/0.19.0/documentation/spimanifest/validation
+// On CI, we want to validate the manifest, but nobody else needs that.
+if ProcessInfo.processInfo.environment.keys.contains("VALIDATE_SPI_MANIFEST") {
+    package.dependencies.append(
+       .package(url: "https://github.com/SwiftPackageIndex/SPIManifest.git", from: "0.12.0")
+    )
+}
