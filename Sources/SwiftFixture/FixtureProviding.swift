7b3f37d9239511af61461e3390873229cb29a145
Liam Nichols <liam.nichols.ln@gmail.com>
2023-07-03 13:26:14 +0200

    Update documentation for Array, Set and Dictionary conformance to FixtureProviding

--- Sources/SwiftFixture/FixtureProviding.swift
+++ Sources/SwiftFixture/FixtureProviding.swift
@@ -28,33 +28,58 @@ public protocol FixtureProviding {
     static func provideFixture(using values: ValueProvider) throws -> Self
 }
 
-// MARK: - Containers
+// MARK: - Conformances
+
+/// `fixture()` support for `Array` types
 extension Array: FixtureProviding {
+    /// Provide a fixture value for use in testing of a given `Array` type.
+    ///
+    /// - Throws: Any error thrown when resolving a fixture for `Element` apart from ``ResolutionError``.
+    /// - Returns: If a fixture can be provided for the `Element` type, an array with a single item will be returned.
+    ///   If `Element` cannot be represented as a fixture because it was not registered, an empty array will be returned instead.
     public static func provideFixture(using values: ValueProvider) throws -> Array<Element> {
-        if let value: Element = try? values.get() {
-            return [value]
-        } else {
+        do {
+            return [try values.get()]
+        } catch is ResolutionError {
             return Array()
+        } catch {
+            throw error
         }
     }
 }
 
+/// `fixture()` support for `Set` types
 extension Set: FixtureProviding {
+    /// Provide a fixture value for use in testing of a given `Set` type.
+    ///
+    /// - Throws: Any error thrown when resolving a fixture for `Element` apart from ``ResolutionError``.
+    /// - Returns: If a fixture can be provided for the `Element` type, a set with a single item will be returned.
+    ///   If `Element` cannot be represented as a fixture because it was not registered, an empty set will be returned instead.
     public static func provideFixture(using values: ValueProvider) throws -> Set<Element> {
-        if let value: Element = try? values.get() {
-            return [value]
-        } else {
+        do {
+            return [try values.get()]
+        } catch is ResolutionError {
             return Set()
+        } catch {
+            throw error
         }
     }
 }
 
+/// `fixture()` support for `Dictionary` types
 extension Dictionary: FixtureProviding {
+    /// Provide a fixture value for use in testing of a given `Dictionary` type.
+    ///
+    /// - Throws: Any error thrown when resolving a fixture for `Key` or `Value` apart from ``ResolutionError``.
+    /// - Returns: If a fixture can be provided for the `Key` and `Value` type, a dictionary with a single entry will be returned.
+    ///   If `Key` or `Value` cannot be represented as a fixture because it was not registered, an empty dictionary will be returned instead.
     public static func provideFixture(using values: ValueProvider) throws -> Dictionary<Key, Value> {
-        if let key: Key = try? values.get(), let value: Value = try? values.get() {
-            return [key: value]
-        } else {
+        do {
+            return [try values.get(): try values.get()]
+        } catch is ResolutionError {
             return Dictionary()
+        } catch {
+            throw error
         }
     }
 }
